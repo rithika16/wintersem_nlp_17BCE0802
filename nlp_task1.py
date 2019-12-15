218b3949dd9d1476617e982706c12a0ceac962ae
rithika16 <51432267+rithika16@users.noreply.github.com>
2019-12-15 12:19:10 +0530

    Add files via upload

---
+++ nlp_task1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+a="shakespeare gave the world best of theatrics"
+print(a)
+b='''William Shakespeare (bapt. 26 April 1564 – 23 April 1616)[a] was an English poet, playwright, and actor, widely regarded as the greatest writer in the English language and the world's greatest dramatist.[2][3][4] He is often called England's national poet and the "Bard of Avon" (or simply "the Bard").[5][b] His extant works, including collaborations, consist of some 39 plays,[c] 154 sonnets, two long narrative poems, and a few other verses, some of uncertain authorship. His plays have been translated into every major living language and are performed more often than those of any other playwright'''
+print(b)
+print(a+b)
+c='''जय गणेश गिरिजा सुवन मंगल करण कृपाल ।
+दीनन के दुःख दूर करि  कीजै नाथ निहाल ॥
+जय जय श्री शनिदेव प्रभु  सुनहु विनय महाराज ।
+करहु कृपा हे रवि तनय राखहु जन की लाज ॥'''
+print(c)
